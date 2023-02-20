f5b1c7c5595486509fa9572fea427ee12a1d4632
unknown <home_dv@mail.ru>
2023-02-20 19:23:52 +0300

    feat: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,30 @@
 class GuessingGame {
     constructor() {}
 
+    currentRange = [];
     setRange(min, max) {
+        for(let i = min; i <= max; i++) {
+            this.currentRange.push(i);
+        }
+    }
 
+    calcMid() {
+        return Math.floor(this.currentRange.length / 2);
     }
 
     guess() {
-
+        const mid = this.calcMid();
+        return this.currentRange[mid];
     }
 
     lower() {
-
+        const mid =  this.calcMid();
+        this.currentRange = this.currentRange.slice(0, mid + 1);
     }
 
     greater() {
-
+        const mid =  this.calcMid();
+        this.currentRange = this.currentRange.slice(mid, this.currentRange.length);
     }
 }
 
